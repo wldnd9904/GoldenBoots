b350cc77c66f421d6565dd61fd4a0218d4b9058c
wldnd9904 <wldnd9904@uos.ac.kr>
2022-11-06 18:22:12 +0900

    order

--- mr-daebak/src/routes/Order.tsx
+++ mr-daebak/src/routes/Order.tsx
@@ -7,11 +7,31 @@ import Header from "../Header";
 
 const Container = styled.div`
     padding: 0px 20px;
-    max-width:480px;
+    width:880px;
     margin:0 auto;
     position:relative;
-    background-color: white;
+    background:teal;
 `;
+
+const MenuList = styled.div`
+    display:block;
+`;
+
+const MenuView = styled.div<IMenuView>`
+    position: relative;
+    display:inline-block;
+    width:250px;
+    height:250px;
+    background-color: tomato;
+    border:1px solid;
+    margin: 15px;
+`;
+
+interface IMenuView{
+    name: string;
+    image: string;
+    key: number;
+}
 
 function Order(){
     return (<>
@@ -19,65 +39,20 @@ function Order(){
                 <title>주문하기</title>
             </Helmet>
         <Container>
-            <ul>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-                <li>order</li>
-            </ul>
+            <MenuList>
+                <MenuView name="asdf" image="a" key={1}>1212312312312313</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>1212312312312313</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>1212312312312313</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+                <MenuView name="asdf" image="a" key={1}>123</MenuView>
+            </MenuList>
         </Container>
         </>
     )
