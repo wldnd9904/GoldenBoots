32e38a77bae8b3d7181c744a224ebd00916151a6
wldnd9904 <wldnd9904@uos.ac.kr>
2022-11-17 20:06:14 +0900

    m

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
-app.use(express.static(path.join(__dirname, "mr-daebak/build")));
+app.use(express.static(path.join(__dirname, "./mr-daebak/build")));
 
-app.get("*", function (요청, 응답) {
-  응답.sendFile(path.join(__dirname, "/mr-daebak/build/index.html"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./mr-daebak/build/index.html"));
+  console.log(req);
 });
