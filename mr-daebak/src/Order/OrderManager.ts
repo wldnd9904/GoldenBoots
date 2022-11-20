96f9a0deae97ad88f0fdb729155091305fd85a49
wldnd9904 <wldnd9904@uos.ac.kr>
2022-11-20 23:57:45 +0900

    ordermanager

--- mr-daebak/src/Order/OrderManager.ts
+++ mr-daebak/src/Order/OrderManager.ts
@@ -2,7 +2,18 @@ import { atom } from "recoil";
 import { IOrder } from "./Order";
 
 export default class OrderManager{
-
+    public static removeOrder(orderID:string):void{
+        
+    }
+    public static sendOrder(orderID:string):void{
+        
+    }
+    public static editOrder(orderID:string, orderData:IOrder):void{
+        
+    }
+    public static cancelOrder(orderID:string):void{
+        
+    }
 }
 
 export const orderListAtom = atom<IOrder[]>({
