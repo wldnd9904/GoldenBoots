a1f28eb25908937cd689c21e299dbd86064b8bce
wldnd9904 <wldnd9904@uos.ac.kr>
2022-11-20 23:59:49 +0900

    peoplemanager

--- mr-daebak/src/People/PeopleManager.ts
+++ mr-daebak/src/People/PeopleManager.ts
@@ -6,6 +6,22 @@ export default class PeopleManager{
     public static async login(id:string, pw:string): Promise<IPeople>{
         return getUserData(id, pw);
     }
+
+    public static logout():void{
+
+    }
+
+    public static editUserData():void{
+
+    }
+
+    public static register():void{
+
+    }
+
+    public static removeUserData(userID:string):void{
+
+    }
 }
 
 export const userDataAtom = atom<IPeople>({
