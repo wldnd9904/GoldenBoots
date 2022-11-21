d5730bd49fabee4c7cdae4ea9380c7a4cf53f2ec
straipe <straipe@naver.com>
2022-11-21 15:56:33 +0000

    대단한 발전?

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const sessionObj={
 app.use(session(sessionObj));
 
 app.get("/", function (req, res) {
-  console.log(req)
+  console.log(res)
   res.sendFile(path.join(__dirname, "/mr-daebak/build/index.html"));
 
 });
@@ -74,24 +74,106 @@ app.post('/register',function(req,res){
   })
 })
 
-/*app.post('/login',function(req,res){
+app.post('/login',function(req,res){
   const userID=req.body.userID
   const password=req.body.password
-
-  var sql_select={userID:userID, password:password}
-  connection.query('select userID from customer where userID=?',[userID],function(err,rows){
+  connection.query('select * from customer where userID=? and password=?',[userID,password], function(err,rows){
     if(rows.length){
-      
+      /*req.session.userID=rows[0].userID
+      req.session.pw=rows[0].pw
+      req.session.isLogined=true;
+      req.session.save(function(){
+        res.json(rows[0])
+      })*/
+      console.log('login ok')
+
+      res.json(rows[0])
     }else{
+      console.log('login fail')
       res.json({'result':'fail'})
-      connection.query('insert into customer set?',sql_insert,function(err,rows){
-        if(err) throw err;
-        console.log('ok')
-        res.json({'result':'ok'})
-      })
     }
   })
-})*/
+})
+// 이벤트 조회
+app.post('/event',function(req,res){
+  connection.query('select * from event',[], function(err,rows){
+    if(rows.length){
+      console.log('event find')
+      res.json(rows)
+    }else{
+      console.log('event fail')
+      res.json({'result':'fail'})
+    }
+  })
+})
+// 상품권 조회
+app.post('/voucher',function(req,res){
+  const userID=req.body.userID
+  connection.query('select * from voucher where userID', [userID], function(err,rows){
+    if(rows.length){
+      console.log('voucher find')
+      res.json(rows)
+    }else{
+      console.log('voucher fail')
+      res.json({'result':'fail'})
+    }
+  })
+})
+
+// 스타일 조회
+app.post('/style',function(req,res){
+  connection.query('select * from style',[], function(err,rows){
+    if(rows.length){
+      console.log('style find')
+      res.json(rows)
+    }else{
+      console.log('style fail')
+      res.json({'result':'fail'})
+    }
+  })
+})
+// 디너 조회
+app.post('/dinner',function(req,res){
+  connection.query('select * from dinner',[], function(err,rows){
+    if(rows.length){
+      console.log('dinner find')
+      res.json(rows)
+    }else{
+      console.log('dinner fail')
+      res.json({'result':'fail'})
+    }
+  })
+})
+
+// 디테일 메뉴 조회
+app.post('/detail',function(req,res){
+  connection.query('select * from detailedmenutype',[], function(err,rows){
+    if(rows.length){
+      console.log('detail find')
+      var tmp = {}
+      rows.map((array)=>tmp[array.name]=array)
+      //console.log(tmp)
+      res.json(tmp)
+    }else{
+      console.log('detail fail')
+      res.json({'result':'fail'})
+    }
+  })
+})
+
+// 주소 조회
+app.post('/address',function(req,res){
+  const userID=req.body.userID
+  connection.query('select * from address where userID=?',[userID], function(err,rows){
+    if(rows.length){
+      console.log('address find')
+      res.json(rows[0])
+    }else{
+      console.log('address fail')
+      res.json({'result':'fail'})
+    }
+  })
+})
 /*app.get("/dbtest",(req,res)=>{
   connection.connect();
   connection.query('SELECT * from customer', (error, rows, fields) => {
