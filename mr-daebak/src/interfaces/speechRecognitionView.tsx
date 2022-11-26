b911e04015bd4894cb41e76e1eff2f612126ffe9
wldnd9904 <wldnd9904@uos.ac.kr>
2022-11-26 21:29:26 +0900

    c

--- mr-daebak/src/interfaces/speechRecognitionView.tsx
+++ mr-daebak/src/interfaces/speechRecognitionView.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-bootstrap/Modal';
-import React from 'react';
+import React, { useEffect } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useRecoilState } from 'recoil';
+import OrderManager, { orderListAtom } from '../Order/OrderManager';
+import MenuManager, { detailListAtom, dinnerListAtom, styleListAtom } from '../Order/MenuManager';
 
 interface IModalForm{
     show: boolean;
@@ -8,13 +11,20 @@ interface IModalForm{
   };
 
 function SpeechRecognitionView({show, handleClose}:IModalForm) {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition()
-
+  const [orderList, setOrderList] = useRecoilState(orderListAtom);
+  const [detailList, setDetailList] = useRecoilState(detailListAtom);
+  const [dinnerList, setDinnerList] = useRecoilState(dinnerListAtom);
+  const [styleList, setStyleList] = useRecoilState(styleListAtom);
+  const {transcript,listening,resetTranscript,browserSupportsSpeechRecognition} = useSpeechRecognition();
+  useEffect(()=>{(async ()=>{
+    if(!detailList)setDetailList(await MenuManager.getDetailedMenuTypeList());
+    if(!dinnerList)setDinnerList(await MenuManager.getDinnerList());
+    if(!styleList)setStyleList(await MenuManager.getStyleList());
+  })();},[]);
+  const stopListening = () =>{
+    SpeechRecognition.stopListening();
+    setOrderList([...orderList,OrderManager.textToOrder(transcript,dinnerList,styleList,detailList)]);
+  }
   return (
         <Modal
         show={show}
@@ -30,7 +40,7 @@ function SpeechRecognitionView({show, handleClose}:IModalForm) {
                 <span>브라우저가 음성 인식을 지원하지 않습니다.</span>
                 :
                 <div>
-                    <p>Microphone: {listening ? 'on' : 'off'}</p>
+                    <p>{listening ? '음성 인식 중' : '대기 중'}</p>
                     <button onClick={()=>SpeechRecognition.startListening({continuous:true, language:"ko"})}>Start</button>
                     <button onClick={SpeechRecognition.stopListening}>Stop</button>
                     <button onClick={resetTranscript}>Reset</button>
